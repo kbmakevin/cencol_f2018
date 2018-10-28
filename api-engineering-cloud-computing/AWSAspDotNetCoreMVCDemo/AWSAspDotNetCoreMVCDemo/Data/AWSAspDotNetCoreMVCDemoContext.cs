8f7299081b10e37f24680c57f61e57c5a49f2346
Kevin Ma <kbma.kevin@gmail.com>
2018-10-28 19:33:20 -0400

    Added AWS ElasticBeanstalk and ASP.NET Core Demo Apps

---
+++ api-engineering-cloud-computing/AWSAspDotNetCoreMVCDemo/AWSAspDotNetCoreMVCDemo/Data/AWSAspDotNetCoreMVCDemoContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AWSAspDotNetCoreMVCDemo.Models
+{
+    public class AWSAspDotNetCoreMVCDemoContext : DbContext
+    {
+        public AWSAspDotNetCoreMVCDemoContext (DbContextOptions<AWSAspDotNetCoreMVCDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AWSAspDotNetCoreMVCDemo.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ api-engineering-cloud-computing/AWSAspDotNetCoreMVCDemo/AWSAspDotNetCoreMVCDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AWSAspDotNetCoreMVCDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
